d8da9d652d97b352ea1fa670003b77adca733510
Ianpyrider <Ianpyrider@users.noreply.github.com>
2025-05-06 20:25:55 -0700

    put glClear in draw

--- Minecraft_clone/glprogram.cpp
+++ Minecraft_clone/glprogram.cpp
@@ -7,17 +7,25 @@ void glProgram::initialize() { //Set up assets (triangle mesh, textures, shaders
 	//Generate shaders
 	m_shader = shaderLoader::createShaderProgram("default.vert", "default.frag");
 
-	//TODO: Initialization steps
+	//TODO: INIT FUNCTIONS
 
 
 }
 
 void glProgram::draw(GLFWwindow* window) { //Render scene (called every frame)
-	//TODO: Draw steps
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//TODO: DRAW FUNCTIONS
 
 
+
+	//Not mentioned in the lab, keep this
+	glfwSwapBuffers(window);
+	glfwPollEvents();
 }
 
 void glProgram::finalize() { //Free resources/memory, called at the end
+	//TODO: FINALIZE FUNCTIONS
+
 
 }
